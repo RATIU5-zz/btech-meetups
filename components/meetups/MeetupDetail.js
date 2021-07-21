3eb8d0f98a243983738793d2edbbd4e1d7ca4382
unknown <jasonmemm@gmail.com>
2021-07-21 08:40:20 -0600

    Added SSG to dynamic routes & fixed images loading

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,10 +1,10 @@
-import { styles } from "ansi-colors";
+import Image from "next/image";
 import classes from "./MeetupDetail.module.css";
 
 function MeetupDetail(props) {
 	return (
 		<section className={classes.detail}>
-			<img src={props.image} alt={props.title} />
+			<Image src={props.image} alt={props.title} width="900" height="700" />
 			<h1>{props.title}</h1>
 			<address>{props.address}</address>
 			<p>{props.description}</p>
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,16 +1,48 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
 	return (
 		<>
 			<MeetupDetail
-				image="https://images.unsplash.com/photo-1626788383851-428d432f8f2e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80"
-				title="A First Meetup"
-				address="Some Street 5, Some City"
-				description="The meetup description"
+				image={props.image}
+				title={props.title}
+				address={props.address}
+				description={props.description}
 			/>
 		</>
 	);
+}
+
+export async function getStaticPaths() {
+	return {
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: "m1",
+				},
+			},
+			{
+				params: {
+					meetupId: "m2",
+				},
+			},
+		],
+	};
+}
+
+export async function getStaticProps(context) {
+	const meetupId = context.params.meetupId;
+
+	return {
+		props: {
+			image: "https://images.unsplash.com/photo-1626788383851-428d432f8f2e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
+			id: meetupId,
+			title: "A First Meetup",
+			address: "Some Street 5, Some City",
+			description: "The meetup description",
+		},
+	};
 }
 
 export default MeetupDetails;
